441a23a8370e148247e642036f05615dfafe2e36
LindseyH <linsnh24@gmail.com>
2025-12-01 21:43:10 -0700

    New project update

--- scripts/index.js
+++ scripts/index.js
@@ -1,13 +1,34 @@
 const editProfileBtn = document.querySelector(".profile__edit-btn");
-
 const editProfileModal = document.querySelector("#edit-profile-modal");
 const editProfileCloseBtn = editProfileModal.querySelector(".modal__close-btn");
+
+const editProfileForm = editProfileModal.querySelector(".modal__form");
+
+const editProfileNameInput = editProfileModal.querySelector(
+  "#profile-name-input"
+);
+const editProfileDescriptionInput = editProfileModal.querySelector(
+  "#profile-description-input"
+);
 
 const newPostBtn = document.querySelector(".profile__post-btn");
 const newPostModal = document.querySelector("#new-post-modal");
 const newPostCloseBtn = newPostModal.querySelector(".modal__close-btn");
 
+const newPostForm = newPostModal.querySelector(".modal__form");
+const newPostCardImageInput = newPostModal.querySelector("#card-image-input");
+const newPostCardCaptionInput = newPostModal.querySelector(
+  "#card-caption-input"
+);
+
+const profileNameEl = document.querySelector(".profile__name");
+const profileDescriptionEl = document.querySelector(".profile__description");
+
+// Trim removes hidden leading spaces/line breaks from profile text.
+// Without trim(), autofilled values looked shifted compared to the placeholder.
 editProfileBtn.addEventListener("click", function () {
+  editProfileNameInput.value = profileNameEl.textContent.trim();
+  editProfileDescriptionInput.value = profileDescriptionEl.textContent.trim();
   editProfileModal.classList.add("modal_is-opened");
 });
 
@@ -22,3 +43,21 @@ newPostBtn.addEventListener("click", function () {
 newPostCloseBtn.addEventListener("click", function () {
   newPostModal.classList.remove("modal_is-opened");
 });
+
+function handleEditProfileSubmit(evt) {
+  evt.preventDefault();
+  profileNameEl.textContent = editProfileNameInput.value;
+  profileDescriptionEl.textContent = editProfileDescriptionInput.value;
+  editProfileModal.classList.remove("modal_is-opened");
+  console.log("submitting");
+}
+
+editProfileForm.addEventListener("submit", handleEditProfileSubmit);
+
+function handleNewPostSubmit(evt) {
+  evt.preventDefault();
+  newPostModal.classList.remove("modal_is-opened");
+  console.log("submitting");
+}
+
+newPostForm.addEventListener("submit", handleNewPostSubmit);
